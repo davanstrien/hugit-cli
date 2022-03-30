f71366d26fe5a5b784a5c1745549256615dc00e7
Daniel van Strien <davanstrien@gmail.com>
2022-03-30 17:23:50 +0100

    tidy core tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,10 +1,5 @@
-<<<<<<< Updated upstream
 """Core tests."""
-=======
-#!/usr/bin/env python3
-from pathlib import Path
 
->>>>>>> Stashed changes
 import pytest
 
 from hugit import core
@@ -23,22 +18,14 @@ def image_files(tmpdir_factory) -> None:
     return a_dir
 
 
-<<<<<<< Updated upstream
 def test_print_frequency_dict():
-    """It runs"""
+    """Print frequency runs."""
     freqs = {"A": 100, "B": 200}
     core.print_table_from_frequency_dict(freqs)
     core.print_table_from_frequency_dict(freqs, sort_by_value=True)
-=======
+
+
 def test_get_images(image_files) -> None:
+    """Test for image files."""
     files = core.get_images(image_files)
     assert files
-
-
-def test_get_image_labels_from_folder() -> None:
-    path = Path("test/label/image_01.png")
-    label = core.get_image_label_from_folder(Path(path))
-    assert label
-    assert isinstance(label, str)
-    assert label == "label"
->>>>>>> Stashed changes
